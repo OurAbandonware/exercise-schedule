030bb0e508a52e6f5c4b297b95b4236c1f0fb934
OurAbandonware <106124689+OurAbandonware@users.noreply.github.com>
2021-12-16 09:20:33 +0000

    9.1.1

--- TradingStrategies/BuyAndHold/BuyAndHold.h
+++ TradingStrategies/BuyAndHold/BuyAndHold.h
@@ -1,1 +1,1 @@
-// This strategy will just buy as much of the stock as possible at the beginning and sell 
+// This strategy will just buy as much of the stock as possible at the beginning and sell a

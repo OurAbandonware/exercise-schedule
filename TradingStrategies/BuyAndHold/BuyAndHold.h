7f01bd8820b80f30380a877f77ab740174ee0ee6
OurAbandonware <106124689+OurAbandonware@users.noreply.github.com>
2021-12-17 14:26:44 +0000

    nouveau: nouveau_set_bo_placement takes TTM flags

--- TradingStrategies/BuyAndHold/BuyAndHold.h
+++ TradingStrategies/BuyAndHold/BuyAndHold.h
@@ -1,1 +1,1 @@
-// This strategy will just buy as much of the stock as possible at the beginning and sell at t
+// This strategy will just buy as much of the stock as possible at the beginning and sell at the
